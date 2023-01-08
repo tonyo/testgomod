0e4b98e4fbb10ba719ce4be24764bb2c8cecd368
Anton Ovchinnikov <anton@tonyo.info>
2023-01-08 14:58:31 +0100

    script for pushing versions

---
+++ push-version.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -euxo pipefail
+
+VERSION=${1}
+
+if [[ $VERSION != v* ]]; then
+    VERSION="v${VERSION}"
+fi
+
+git tag "${VERSION}" || true
+git push origin "${VERSION}"
